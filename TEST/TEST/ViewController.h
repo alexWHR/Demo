85a583edc77a85355f9b1581ef4806105e24d5bb
Alex <812110734@qq.com>
2016-03-30 12:26:21 +0800

    create by Alex 第一次输入

---
+++ TEST/TEST/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TEST
+//
+//  Created by ibokan2 on 16/3/30.
+//  Copyright © 2016年 ibokan2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TEST/TEST/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TEST
+//
+//  Created by ibokan2 on 16/3/30.
+//  Copyright © 2016年 ibokan2. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"test");
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
